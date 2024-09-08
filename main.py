70dac00bc0389bd205167f1f0c273c9f71952aa8
AndresEscobar09 <wilmera.escobarc@ecci.edu.co>
2024-09-07 19:59:23 -0500

    se organizan rutas

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from routers.product import router as product_router
 
 
 app = FastAPI()
-app.include_router(product_router)
-
-
 
 @app.get("/")
 def index():
     return "hola heladeria"
+
+
+app.include_router(product_router)
+
 
 
     
